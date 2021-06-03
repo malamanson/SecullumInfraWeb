8345a99751946526ea7d6b678839a69cba068783
Wiliam Maciel <malamanson@hotmail.com>
2021-06-03 15:01:19 -0300

    initial

---
+++ SecullumInfraWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SecullumInfraWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
